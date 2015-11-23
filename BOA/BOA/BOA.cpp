e2324c212f25c0062a537db6f94d4a7927833fa6
Yu Zhang <whuzhangyu@gmail.com>
2015-11-23 14:49:04 -0500

    sweep line algorithm

---
+++ BOA/BOA/BOA.cpp
@@ -0,0 +1,129 @@
+// BOA.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
+// segments intersections detection
+// sweepline algorithm implemented
+Sweepline Algorithm
+{
+    // initialize event data structure - minimum priority queue
+    // event queue EQ initially stores all the start and end points
+    // of all the line segments, and the endpoints are stored in the
+    // minimum priority queue according to their x coordinates
+    EQ = Heapify(all segment endpoints);
+
+    // initialize sweep line SL to be empty
+    // SL is stored using a binary search tree
+    SL = BST.Build(Null);
+
+    // initialize the output intersection list IL to be empty
+    IL = List.Build(Null);
+
+    while (EQ != Null)
+    {
+        // pop up the root of the EQ to get the next event
+        E = EQ.Pop();
+
+        // if E is the left endpoint of segment
+        if (IsLeftEndPoint(E) == true)
+        {
+            // get the segment of E
+            segE = FindSegID(E);
+            // add segment segE to the sweep line SL
+            SL.Insert(segE);
+            // find the segment above segE in SL, that is the
+            // in-order predecessor of segE in SL
+            segA = SL.FindInOrderPre(segE);
+            // find the segment below segE in SL, that is the
+            // in-order successor of segE in SL
+            segB = SL.FindInOrderAfter(segE);
+            // detect if segE and segA are intersected
+            if ((I = Intersection(segE, segA)) != Null)
+            {
+                // if intersected, add the intersection into EQ
+                EQ.Push(I);
+            }
+            // detect if segE and segB are intersected
+            if ((I = Intersection(segE, segB)) != Null)
+            {
+                // if intersected, add the intersection into EQ
+                EQ.Push(I);
+            }
+        }
+        // if E is the right endpoint of segment
+        else if (IsRightEndPoint(E) == true)
+        {
+            // get the segment of E
+            segE = FindSegID(E);
+            // find the segment above segE in SL
+            segA = SL.FindInOrderPre(segE);
+            // find the segment below segE in SL
+            segB = SL.FindInOrderAfter(segE);
+            // delete segE from SL
+            SL.Remove(segE);
+            // now segA and segB are neighbours
+            // detect if segA and segB are intersected
+            if ((I = Intersection(segA, segB)) != Null)
+            {
+                // if segA and segB are intersected, and 
+                // the intersection I is not in EQ already
+                if (EQ.Find(I) == null)
+                {
+                    // add the intersection into EQ
+                    EQ.Push(I);
+                }
+            }
+        }
+        // if E is an intersection event
+        else
+        {
+            // add E into the output list IL
+            IL.Add(E);
+            // get the upper segment of intersection I
+            segE1 = SL.FindInOrderPre(I);
+            // get the lower segment of intersection I
+            segE2 = SL.FindInOrderAfter(I);
+            // swap their positions so that segE2 is above segE1
+            SL.Swaposition(segE1, segE2);
+            // get the segment above segE2 in SL
+            segA = SL.FindInOrderPre(segE2);
+            // get the segment below segE1 in SL
+            segB = SL.FindInOrderAfter(segE1);
+            // now segA and segE2 are neighbours
+            // detect if segA and segE2 are intersected
+            if ((I = Intersection(segA, segE2)) != Null)
+            {
+                // if segA and segE2 are intersected, and 
+                // the intersection I is not in EQ already
+                if (EQ.Find(I) == null)
+                {
+                    // add the intersection into EQ
+                    EQ.Push(I);
+                }
+            }
+            // now segB and segE1 are neighbours
+            // detect if segB and segE1 are intersected
+            if ((I = Intersection(segB, segE1)) != Null)
+            {
+                // if segB and segE1 are intersected, and 
+                // the intersection I is not in EQ already
+                if (EQ.Find(I) == Null)
+                {
+                    // add the intersection into EQ
+                    EQ.Push(I);
+                }
+            }
+        }
+        // now event E is processed, continue to the next event
+    }
+    // now all the events are processed,
+    // return the intersection list IL
+    return IL;
+}
---
+++ BOA/BOA/ReadMe.txt
@@ -0,0 +1,40 @@
+========================================================================
+    CONSOLE APPLICATION : BOA Project Overview
+========================================================================
+
+AppWizard has created this BOA application for you.
+
+This file contains a summary of what you will find in each of the files that
+make up your BOA application.
+
+
+BOA.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard.
+    It contains information about the version of Visual C++ that generated the file, and
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+BOA.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+BOA.cpp
+    This is the main application source file.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named BOA.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" comments to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////

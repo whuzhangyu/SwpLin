8eaa8156b4d50c2f04554781ba6ffc2de247e7d7
Yu Zhang <whuzhangyu@gmail.com>
2015-11-23 18:27:13 -0500

    fixed more bugs

--- BOA/BOA/BOA.cpp
+++ BOA/BOA/BOA.cpp
@@ -17,10 +17,16 @@ Sweepline Algorithm
     // event queue EQ initially stores all the start and end points
     // of all the input line segments, and the endpoints are stored in the
     // minimum priority queue according to their x coordinates
+    // each node of EQ is a data structure consists of the coordinates of
+    // the point, and two IDs (seg1, seg2) of the two segments which
+    // intersect at the point, if the point is not an intersection point,
+    // then it only stores the ID of the segment which it belongs to
     EQ = Heapify(all segment endpoints);
 
     // initialize sweep line data structure SL to be empty
-    // SL is stored using a binary search tree class
+    // SL is a binary search tree class, stores the active segments
+    // according to the y coordinates of their intersections with the
+    // sweep line
     SL = new class BST(Null);
 
     // initialize the output intersection array list IL to be empty
@@ -29,7 +35,8 @@ Sweepline Algorithm
     while (EQ != Null)
     {
         // pop up the root of the EQ to get the next event
-        // that's the left-most event point
+        // that's the left-most event point, this means the sweep line
+        // moves from left to right
         E = EQ.PopRoot();
 
         // if E is the left endpoint of segment
@@ -40,15 +47,15 @@ Sweepline Algorithm
             // add segment segE to the sweep line SL
             SL.Insert(segE);
             // find the segment above segE in SL, that is the
-            // in-order predecessor of segE in SL
-            segA = SL.FindInOrderPre(segE);
-            // find the segment below segE in SL, that is the
             // in-order successor of segE in SL
-            segB = SL.FindInOrderAfter(segE);
+            segA = SL.FindInOrderNext(segE);
+            // find the segment below segE in SL, that is the
+            // in-order predecessor of segE in SL
+            segB = SL.FindInOrderPre(segE);
             // detect if segE and segA are intersected
             if ((I = Intersection(segE, segA)) != Null)
             {
-                // if intersected, add the intersection into EQ
+                // if intersected, add the intersection I into EQ
                 EQ.Push(I);
             }
             // detect if segE and segB are intersected
@@ -64,9 +71,9 @@ Sweepline Algorithm
             // get the segment of E
             segE = FindSegID(E);
             // find the segment above segE in SL
-            segA = SL.FindInOrderPre(segE);
+            segA = SL.FindInOrderNext(segE);
             // find the segment below segE in SL
-            segB = SL.FindInOrderAfter(segE);
+            segB = SL.FindInOrderPre(segE);
             // delete segE from SL
             SL.Remove(segE);
             // now segA and segB are neighbours
@@ -77,7 +84,7 @@ Sweepline Algorithm
                 // the intersection I is not in EQ already
                 if (EQ.Find(I) == null)
                 {
-                    // add the intersection into EQ
+                    // add the intersection I into EQ
                     EQ.Push(I);
                 }
             }
@@ -87,16 +94,17 @@ Sweepline Algorithm
         {
             // add E into the output list IL
             IL.Add(E);
-            // get the upper segment of intersection I
-            segE1 = SL.FindInOrderPre(I);
+            // get the upper segment among the two segments that intersect
+            // at E, that's the right one in the in-order traversal of SL
+            segE1 = SL.InOrderRightOne(E.seg1, E.seg2);
             // get the lower segment of intersection I
-            segE2 = SL.FindInOrderAfter(I);
+            segE2 = SL.InOrderLeftOne(E.seg1, E.seg2);
             // swap their positions so that segE2 is above segE1
             SL.Swaposition(segE1, segE2);
             // get the segment above segE2 in SL
-            segA = SL.FindInOrderPre(segE2);
+            segA = SL.FindInOrderNext(segE2);
             // get the segment below segE1 in SL
-            segB = SL.FindInOrderAfter(segE1);
+            segB = SL.FindInOrderPre(segE1);
             // now segA and segE2 are neighbours
             // detect if segA and segE2 are intersected
             if ((I = Intersection(segA, segE2)) != Null)
